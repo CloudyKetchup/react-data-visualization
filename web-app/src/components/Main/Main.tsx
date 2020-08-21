b471cd1a094ad2dda79788fd25455d1867b56def
CloudyKetchup <maxdodon25@gmail.com>
2020-08-21 18:11:08 +0100

    mounted FiltersPanel and DataContainer

--- web-app/src/components/Main/Main.tsx
+++ web-app/src/components/Main/Main.tsx
@@ -1,15 +1,33 @@
 import React, { FC } from "react";
 
 import styled from "styled-components";
+import DataContainer from "../DataContainer/DataContainer";
+import FiltersPanel from "../FiltersPanel/FiltersPanel";
+import { FiltersProvider } from "../../context/FiltersContext";
+import Header from "../Header/Header";
 
 const Main = styled.div`
+  background: #e6e9ed;
   height: 100%;
   width: 100%;
+  position: relative;
+  overflow: hidden;
 `;
 
 const MainContainer: FC = () => (
   <Main>
-
+    <Header/>
+    <div style={{
+      height: "calc(100% - 50px)",
+      width: "100%",
+      marginTop: "50px",
+      paddingTop: "10px",
+    }}>
+      <FiltersProvider>
+        <FiltersPanel />
+        <DataContainer />
+      </FiltersProvider>
+    </div>
   </Main>
 );
 
