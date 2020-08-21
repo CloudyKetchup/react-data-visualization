cb7c0c6c07c69c09e971cf1836588b9e0ab8e9f5
CloudyKetchup <maxdodon25@gmail.com>
2020-08-21 22:00:57 +0100

    LineGraphComponent

---
+++ web-app/src/components/LineGraph/LineGraph.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const LineGraphStyles = styled.div`
+  height: 100%;
+  width: 100%;
+`;
+
+//TODO: finish
+const LineGraph = () => {
+  
+  return (
+    <LineGraphStyles>
+      
+    </LineGraphStyles>
+  );
+}
+
+export default LineGraph;
