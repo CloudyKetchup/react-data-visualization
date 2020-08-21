c0aeb33ce78a0a6bcc52b86488d700cd6e02e3b2
CloudyKetchup <maxdodon25@gmail.com>
2020-08-21 22:01:11 +0100

    mounted graph components

--- web-app/src/components/DataContainer/DataContainer.tsx
+++ web-app/src/components/DataContainer/DataContainer.tsx
@@ -1,24 +1,33 @@
-import React, { FC } from "react";
-import styled from "styled-components";
-import FiltersPane from "../../atoms/FiltersPane";
+import React, { FC, useEffect } from "react";
+
 import { useRecoilState } from "recoil";
+import FiltersPane        from "../../atoms/FiltersPane";
+
+import TableGraph from "../TableGraph/TableGraph";
+import LineGraph  from "../LineGraph/LineGraph";
+
+import styled from "styled-components";
 
 const DataContainerStyles = styled.div`
+  display: grid;
+  grid-gap: 20px;
   height: 100%;
+  padding: 20px;
   float: right;
   background: white;
   transition: 0.2s;
 `;
 
-const DataContainer: FC = props => {
+const DataContainer: FC = () => {
   const [filtersPane] = useRecoilState(FiltersPane);
 
   return (
     <DataContainerStyles
       className="material-pane"
-      style={{ width: filtersPane ? "calc(100% - 310px)" : "100%" }}
+      style={{ width: filtersPane ? "calc(100% - 350px)" : "calc(100% - 40px)" }}
     >
-
+      <TableGraph/>
+      <LineGraph/>
     </DataContainerStyles>
   )
 };
