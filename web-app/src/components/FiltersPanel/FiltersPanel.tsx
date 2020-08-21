38a3364ac56e1a1b1d1677faa446fd8beaed6a3c
CloudyKetchup <maxdodon25@gmail.com>
2020-08-21 20:44:02 +0100

    moved Dropdown to separate file

--- web-app/src/components/FiltersPanel/FiltersPanel.tsx
+++ web-app/src/components/FiltersPanel/FiltersPanel.tsx
@@ -1,95 +1,24 @@
-import React, { FC, useState } from "react";
-import { useRecoilState } from "recoil";
-import FiltersPane from "../../atoms/FiltersPane";
+import React, { FC } from "react";
 
-import { ReactComponent as ArrowSVG } from "../../assets/icons/down-arrow.svg";
+import { useRecoilState } from "recoil";
+import FiltersPane        from "../../atoms/FiltersPane";
+
+import YearsFilter      from "./YearsFilter";
+import SeveritiesFilter from "./SeverinitiesFilter";
 
 import {
   FiltersStyles,
   FiltersContainer,
-  Filter,
-  FilterTitle,
   Footer,
-  DropdownStyles,
-  DropdownItems,
-  DropdownItem,
-  DropdownHeader
+  GenerateGraph,
 } from "./styles";
-
-type IDropdownItem = {
-  title: string,
-  selected?: boolean,
-  onClick: () => void
-};
-
-type IDropdownProps = {
-  placeholder: string,
-  items: IDropdownItem[],
-  selectMultiple?: boolean
-};
-
-const Dropdown: FC<IDropdownProps> = ({
-  placeholder,
-  items,
-  selectMultiple = false
-}) => {
-  const [expanded, setExpanded] = useState<boolean>(false);
-
-  const onItemClick = (item: IDropdownItem) => {
-    item.onClick();
-
-    !selectMultiple && setExpanded(false);
-  };
-
-  return (
-    <DropdownStyles onClick={() => setExpanded(!expanded)}>
-      <DropdownHeader>
-        <div>
-          <span>{placeholder}</span>
-        </div>
-        <div style={{ marginLeft: "auto", marginTop: "5px" }}>
-          <ArrowSVG fill="gray" height="20px" width="20px"/>
-        </div>
-      </DropdownHeader>
-      {
-        expanded
-        &&
-        <DropdownItems>
-          {items.map(item =>
-            <DropdownItem key={item.title} onClick={() => onItemClick(item)}>
-              <span style={{
-                fontWeight: item.selected ? "bold" : "normal"
-              }}>
-                {item.title}
-              </span>
-            </DropdownItem>
-          )}
-        </DropdownItems>
-      }
-    </DropdownStyles>
-  )
-};
 
 const FiltersPanel: FC = () => {
   const [filtersPane] = useRecoilState(FiltersPane);
 
-  //TODO: actions and selected
-  const years: IDropdownItem[] = [
-    {
-      title: "2016",
-      selected: true,
-      onClick: () => {}
-    },
-    {
-      title: "2017",
-      onClick: () => {}
-    },
-    {
-      title: "2018",
-      onClick: () => {}
-    }
-  ];
-  const severinites: IDropdownItem[] = [];
+  const generateGraph = () => {
+    //TODO: implement
+  };
 
   return (
     <FiltersStyles
@@ -97,24 +26,14 @@ const FiltersPanel: FC = () => {
       style={{ marginLeft: filtersPane ? "0" : "-300px" }}
     >
       <FiltersContainer>
-        <Filter>
-          <FilterTitle>
-            <h5>Select a single year</h5>
-          </FilterTitle>
-          <Dropdown placeholder="Select year" items={years} />
-        </Filter>
-        <Filter>
-          <FilterTitle>
-            <h5>Select multiple severities</h5>
-          </FilterTitle>
-          <Dropdown
-            placeholder="Select severity"
-            items={severinites}
-            selectMultiple
-          />
-        </Filter>
+        <YearsFilter/>
+        <SeveritiesFilter/>
       </FiltersContainer>
-      <Footer/>
+      <Footer>
+        <GenerateGraph className="material-pane" onClick={generateGraph}>
+          Generate Graph
+        </GenerateGraph>
+      </Footer>
     </FiltersStyles>
   );
 };
