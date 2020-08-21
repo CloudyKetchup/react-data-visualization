c5daed6ce6cdf81f7860d39d201e6191b41e6116
CloudyKetchup <maxdodon25@gmail.com>
2020-08-21 22:00:27 +0100

    TableGraph component

---
+++ web-app/src/components/TableGraph/TableGraph.tsx
@@ -0,0 +1,45 @@
+import React, { FC } from "react";
+
+import { useRecoilState } from "recoil";
+import { GraphDataAtom } from "../../atoms/GraphData";
+
+import { AgGridReact } from 'ag-grid-react';
+
+import styled from "styled-components";
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+const TableGraphStyles = styled.div`
+  height: 100%;
+  width: 100%;
+  overflow: auto;
+`;
+
+const TableGraph: FC = () => {
+  const [graphData] = useRecoilState(GraphDataAtom);
+
+  const columns = [
+    { headerName: "CVE ID", field: "id" },
+    { headerName: "PUBLISHED DATE", field: "published_date" },
+    { headerName: "SEVERITY", field: "severity" },
+    { headerName: "DESCRIPTION", field: "description" }
+  ];
+
+  const rows = graphData?.data.map(cve => ({
+    id: cve.CVE_data_meta.ID,
+    published_date: cve.impact.publishedDate,
+    severity: cve.impact.baseMetricV2.severity,
+    description: cve.description.description_data[0].value
+  }));
+ 
+  return (
+    <TableGraphStyles className="ag-theme-alpine">
+      <AgGridReact
+        columnDefs={columns}
+        rowData={rows}
+      />
+    </TableGraphStyles>
+  );
+}
+
+export default TableGraph;
