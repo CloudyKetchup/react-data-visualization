a9776cfee4d7231d27ed37d73ed105c20ea852e8
CloudyKetchup <maxdodon25@gmail.com>
2020-08-22 12:25:14 +0100

    finished "generateGraph" button action

--- web-app/src/components/FiltersPanel/FiltersPanel.tsx
+++ web-app/src/components/FiltersPanel/FiltersPanel.tsx
@@ -2,9 +2,14 @@ import React, { FC } from "react";
 
 import { useRecoilState } from "recoil";
 import FiltersPane        from "../../atoms/FiltersPane";
+import { GraphDataAtom }  from "../../atoms/GraphData";
+import { YearsFilterAtom, SeveritiesFilterAtom } from "../../atoms/Filters";
+import { Severity } from "../../models/CVE";
 
 import YearsFilter      from "./YearsFilter";
 import SeveritiesFilter from "./SeverinitiesFilter";
+
+import { getByFilters } from "../../api/GraphApi";
 
 import {
   FiltersStyles,
@@ -14,10 +19,20 @@ import {
 } from "./styles";
 
 const FiltersPanel: FC = () => {
-  const [filtersPane] = useRecoilState(FiltersPane);
+  const [filtersPane]       = useRecoilState(FiltersPane);
+  const [yearsFilter]       = useRecoilState(YearsFilterAtom);
+  const [severitiesFilter]  = useRecoilState(SeveritiesFilterAtom);
+  const [_, setGraphData]   = useRecoilState(GraphDataAtom);
 
-  const generateGraph = () => {
-    //TODO: implement
+  const generateGraph = async () => {
+    const severities = severitiesFilter.map(severity => severity.severity.toUpperCase() as Severity)
+
+    const { data } = await getByFilters({
+      severities: severities,
+      year: yearsFilter?.year
+    })
+
+    data && setGraphData({ data: data });
   };
 
   return (
